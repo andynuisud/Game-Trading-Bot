344c5e2ea051b28e962d1bd37cf2701f5455a574
Andrew Nuisud <110713868+andynuisud@users.noreply.github.com>
2025-07-24 13:03:27 -0400

    Create Config.py

---
+++ Config.py
@@ -0,0 +1,9 @@
+ROLIMONS_API = "https://www.rolimons.com/itemapi/itemdetails"
+TRADES_API = "https://api.roblox.com/trades"
+COOKIE = "YOUR_ROBLOSECURITY_COOKIE"
+PROFIT_THRESHOLD = 100
+
+HEADERS = {
+    'User-Agent': 'TradeBot/1.0',
+    'Cookie': f'.ROBLOSECURITY={COOKIE}'
+}
